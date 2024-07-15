7d67ec7cf15bd54f02af4d2b1f31d335e6090a1a
NANKXIN <1244170086@qq.com>
2024-07-15 22:35:19 +0800

    FreeRTOS复习
    任务：状态、延时、空闲任务和钩子函数

---
+++ 00_嵌入式/00_MCU/00_基础/01_FreeRTOS入门/01_project/01_idle_none/Demo/CORTEX_STM32F103_Keil/main.c
@@ -0,0 +1,217 @@
+/*
+ * FreeRTOS V202212.01
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ */
+
+/*
+ * Creates all the demo application tasks, then starts the scheduler.  The WEB
+ * documentation provides more details of the standard demo application tasks.
+ * In addition to the standard demo tasks, the following tasks and tests are
+ * defined and/or created within this file:
+ *
+ * "Fast Interrupt Test" - A high frequency periodic interrupt is generated
+ * using a free running timer to demonstrate the use of the
+ * configKERNEL_INTERRUPT_PRIORITY configuration constant.  The interrupt
+ * service routine measures the number of processor clocks that occur between
+ * each interrupt - and in so doing measures the jitter in the interrupt timing.
+ * The maximum measured jitter time is latched in the ulMaxJitter variable, and
+ * displayed on the LCD by the 'Check' task as described below.  The
+ * fast interrupt is configured and handled in the timertest.c source file.
+ *
+ * "LCD" task - the LCD task is a 'gatekeeper' task.  It is the only task that
+ * is permitted to access the display directly.  Other tasks wishing to write a
+ * message to the LCD send the message on a queue to the LCD task instead of
+ * accessing the LCD themselves.  The LCD task just blocks on the queue waiting
+ * for messages - waking and displaying the messages as they arrive.
+ *
+ * "Check" task -  This only executes every five seconds but has the highest
+ * priority so is guaranteed to get processor time.  Its main function is to
+ * check that all the standard demo tasks are still operational.  Should any
+ * unexpected behaviour within a demo task be discovered the 'check' task will
+ * write an error to the LCD (via the LCD task).  If all the demo tasks are
+ * executing with their expected behaviour then the check task writes PASS
+ * along with the max jitter time to the LCD (again via the LCD task), as
+ * described above.
+ *
+ */
+
+/* Standard includes. */
+#include <stdio.h>
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+/* Library includes. */
+#include "stm32f10x_it.h"
+
+#include "serial.h"
+
+
+static void prvSetupHardware( void );
+static TaskHandle_t startHandle = NULL;
+static TaskHandle_t deleteHandle = NULL;
+
+
+static StaticTask_t Idle_Task_TCB;
+static StackType_t Idle_Task_Stack[configMINIMAL_STACK_SIZE];
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize)
+{
+    *ppxIdleTaskTCBBuffer = &Idle_Task_TCB; /* 任务控制块内存 */
+    *ppxIdleTaskStackBuffer = Idle_Task_Stack; /* 任务堆栈内存 */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE; /* 任务堆栈大小 */
+}
+
+
+static void deleteTask( void *pvParameters )
+{
+	while(1)
+	{
+		printf("deleteTask.\r\n");
+		vTaskDelete(NULL);
+	}
+}
+
+static void StartTask( void *pvParameters )
+{
+	BaseType_t xRetrun;
+	
+	while (1)
+	{
+		xRetrun = xTaskCreate(deleteTask, "delete", 1024, NULL, 2, &deleteHandle);
+		if (xRetrun != pdPASS)
+		{
+			printf("xTaskCreate err.\r\n");
+		}
+	}
+}
+
+static StaticTask_t staticHandle;
+static StackType_t staticBuf[100];
+static void StaticTask( void *pvParameters )
+{
+	while (1)
+	{
+		printf("Static Task.\r\n");
+		vTaskDelay(500);
+	}
+}
+
+int main( void )
+{
+#ifdef DEBUG
+  debug();
+#endif
+
+	prvSetupHardware();
+	
+	printf("Hello World.\r\n");
+	
+	taskENTER_CRITICAL();
+	xTaskCreate(StartTask, "Start", 100, NULL, 1, &startHandle);
+	xTaskCreateStatic(StaticTask, "Static", 100, NULL, 1, staticBuf, &staticHandle);
+	taskEXIT_CRITICAL();
+	
+	vTaskStartScheduler();
+	
+	return 0;
+}
+
+static void prvSetupHardware( void )
+{
+	/* Start with the clocks in their expected state. */
+	RCC_DeInit();
+
+	/* Enable HSE (high speed external clock). */
+	RCC_HSEConfig( RCC_HSE_ON );
+
+	/* Wait till HSE is ready. */
+	while( RCC_GetFlagStatus( RCC_FLAG_HSERDY ) == RESET )
+	{
+	}
+
+	/* 2 wait states required on the flash. */
+	*( ( unsigned long * ) 0x40022000 ) = 0x02;
+
+	/* HCLK = SYSCLK */
+	RCC_HCLKConfig( RCC_SYSCLK_Div1 );
+
+	/* PCLK2 = HCLK */
+	RCC_PCLK2Config( RCC_HCLK_Div1 );
+
+	/* PCLK1 = HCLK/2 */
+	RCC_PCLK1Config( RCC_HCLK_Div2 );
+
+	/* PLLCLK = 8MHz * 9 = 72 MHz. */
+	RCC_PLLConfig( RCC_PLLSource_HSE_Div1, RCC_PLLMul_9 );
+
+	/* Enable PLL. */
+	RCC_PLLCmd( ENABLE );
+
+	/* Wait till PLL is ready. */
+	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+	{
+	}
+
+	/* Select PLL as system clock source. */
+	RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK );
+
+	/* Wait till PLL is used as system clock source. */
+	while( RCC_GetSYSCLKSource() != 0x08 )
+	{
+	}
+
+	/* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
+	RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
+							| RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO, ENABLE );
+
+	/* SPI2 Periph clock enable */
+	RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
+
+
+	/* Set the Vector Table base address at 0x08000000 */
+	NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
+
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+
+	/* Configure HCLK clock as SysTick clock source. */
+	SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
+	
+	SerialPortInit();
+}
+
+/*-----------------------------------------------------------*/
+
+#ifdef  DEBUG
+/* Keep the linker happy. */
+void assert_failed( unsigned char* pcFile, unsigned long ulLine )
+{
+	for( ;; )
+	{
+	}
+}
+#endif
--- 00_嵌入式/00_MCU/00_基础/01_FreeRTOS入门/FreeRTOS入门.md
+++ 00_嵌入式/00_MCU/00_基础/01_FreeRTOS入门/FreeRTOS入门.md
@@ -20,6 +20,7 @@ int fputc( int ch, FILE *f )
 }
 ```
 4. 配置模拟器
+	==注意 DLL 和 Parameter 和选择的芯片一致==
 	![500](./00_pic/00_裁剪官方例程/p6.png)
 5. 打印输出
 	![](./00_pic/00_裁剪官方例程/p7.png)
@@ -65,8 +66,126 @@ int fputc( int ch, FILE *f )
 | `pdPASS`  | `1` |
 | `pdFAIL`  | `0` |
 
-# 四. 内存管理
+# 四. 任务
+## 4.1 动态任务
+- 多个任务可调用同一个任务函数，每个任务栈独立，运行互不影响
+```C
+static void StartTask( void *pvParameters )
+{
+	while (1)
+	{
+		printf("Start Task.\r\n");
+		vTaskDelay(500);
+	}
+}
+xTaskCreate(StartTask, "Start", 100, NULL, 1, &startHandle);
+```
 
+## 4.2 静态任务
+工程路径：`01_project\00_task_static`
+
+- 打开 `configSUPPORT_STATIC_ALLOCATION`
+- 需实现空闲任务内存分配函数 `vApplicationGetIdleTaskMemory`
+- 多个任务可调用同一个任务函数，每个任务栈独立，运行互不影响
+```C
+// 打开静态内存
+#define configSUPPORT_STATIC_ALLOCATION    1
+
+// 空闲任务
+static StaticTask_t idleHandle;
+static StackType_t IdleStack[configMINIMAL_STACK_SIZE];
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
+{
+    *ppxIdleTaskTCBBuffer = &idleHandle;
+    *ppxIdleTaskStackBuffer = IdleStack;
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+// 任务创建
+#define STATS_TASK_SIZE		( 100 )
+static StaticTask_t staticHandle;
+static StackType_t staticStack[STATS_TASK_SIZE];
+static void StaticTask(void *pvParameters)
+{
+	while (1)
+	{
+		printf("Static Task.\r\n");
+		vTaskDelay(500);
+	}
+}
+xTaskCreateStatic(StaticTask, "Static", STATS_TASK_SIZE, NULL, 1, staticStack, &staticHandle);
+```
+
+## 4.3 删除任务
+```C
+vTaskDelete(NULL);  		// 删除自身
+vTaskDelete(&otherHandle);  // 删除其它任务
+```
+
+## 4.4 系统中断
+- FreeRTOS在系统滴答中断内切换任务
+- 同优先级任务的时间片 = 滴答定时器的周期时间（如：滴答1ms，各任务轮流运行1ms）
+- FreeRTOS 内用 `pdMS_TO_TICKS` 将 ms 转换为 tick，如：`vTaskDelay(pdMS_TO_TICKS(50))`延时 50ms 对应的 tick
+![500](./00_pic/04_任务/p1.png)
+
+## 4.5 任务状态
+1. 就绪状态(Ready)：**任务刚创建时**、任务运行时被高优先级打断、结束阻塞/暂停时
+2. 运行状态(Running)：任务正在运行
+3. 阻塞状态(Blocked)：等待某事发生，如：等待队列、系统延时
+4. 暂停状态(Suspended)：主动休息，唯一方法：`vTaskSuspend`；退出暂停，只能由其它任务：`vTaskResume`
+
+![500](./00_pic/04_任务/p2.png)
+```C
+vTaskSuspend(NULL);  		 // 暂停自身
+vTaskSuspend(&otherHandle);  // 暂停其它任务
+vTaskResume(&otherHandle);   // 恢复其它任务
+```
+
+## 4.6 延时
+```C
+vTaskDelay(n);				// 相对时间
+vTaskDelayUntil(&pre, n);	// 绝对时间，到时间后会更新 pre = pre + n
+```
+![](./00_pic/04_任务/p3.png)
+
+## 4.7 空闲任务及其钩子函数
+### 4.7.1 空闲任务
+1. 特点：
+	1. 优先级为0
+	2. 只能处于就绪态和运行态，永不堵塞
+2. 作用：
+	1. 释放被删除的任务的内存（==删除自身的情况==），若用户任务永不堵塞，空闲任务将无法运行，也无法释放内存
+		![](./00_pic/04_任务/p4.png)
+	2. 调度器必须有一个可运行的函数
+
+例：关闭空闲任务
+工程路径：`01_project\01_idle_none`
+![](./00_pic/04_任务/p5.png)
+![](./00_pic/04_任务/p6.png)
+
+### 4.7.2 钩子函数
+使能：
+![](./00_pic/04_任务/p7.png)
+ 注意：
+> 1. 执行一些低优先级的、后台的、需要连续执行的函数
+> 2. 测量系统的空闲时间：计算处理器占用率
+> 3. 让系统进入省电模式
+
+例：使能空闲任务
+工程路径：`01_project\02_idle_hook`
+1. 用户任务不堵塞：空闲任务无法运行
+	![](./00_pic/04_任务/p8.png)
+	![](./00_pic/04_任务/p9.png)
+2. 用户任务堵塞：空闲任务可运行释放内存
+	![](./00_pic/04_任务/p10.png)
+	![](./00_pic/04_任务/p11.png)
+
+## 4. 优先级
+> 优先级范围：`0~(configMAX_PRIORITIES – 1)`，*==越大优先级越高==*
+
+默认调度机制：
+1. 高优先级任务先运行，同等任务轮流运行
+2. 若高优先级任务不主动放弃运行，低优先级任务无法运行
 
 
 
